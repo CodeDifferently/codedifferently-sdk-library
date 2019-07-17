6d98c9cceffcbfdf457cbb6012de9667c29a3598
CodeDifferently <leonhunter@Leons-MacBook-Pro-2.local>
2019-07-17 13:13:00 -0400

    updagte

--- windows-sdk/installer.bat
+++ windows-sdk/installer.bat
@@ -1,6 +1,6 @@
 :: install `7zip` installer to unzip projects from command line
 echo "downloading 7zip installer..."
-curl -o 7zip-installer.exe https://www.7-zip.org/a/7z1900-x64.exe
+curl -o "7zip-installer.exe" "https://www.7-zip.org/a/7z1900-x64.exe"
 
 echo "Installing 7zip"
 start 7zip-installer.exe
@@ -9,10 +9,14 @@ start 7zip-installer.exe
 
 
 
+
+
+
+
+
 :: install `Git Bash` installer to clone projects from command line
 echo "downloading Git-Bash installer..."
-curl -o gitbash-installer.exe https://github.com/git-for-windows/git/releases/download/v2.22.0.windows.1/Git-2.22.0-64-bit.exe
-
+curl -o "gitbash-installer.exe" "https://github.com/git-for-windows/git/releases/download/v2.22.0.windows.1/Git-2.22.0-64-bit.exe"
 
 
 echo "Installing GitBash"
@@ -25,9 +29,69 @@ start gitbash-installer.exe
 
 
 
+
+:: download `Apache2` installer
+echo "downloading Apache installer"
+curl -o "apache-installer.msi" "http://archive.apache.org/dist/httpd/binaries/win32/apache_2.2.9-win32-x86-openssl-0.9.8h-r2.msi"
+
+echo "Unzipping Apache Installer"
+start apache-installer.msi
+:: -----------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+:: `WebPlatformInstaller` is a PHP installer tool for windows
+echo "downloading WebPlatformInstaller installer"
+curl -o "webplatforminstaller-installer.msi" "https://download.microsoft.com/download/8/4/9/849DBCF2-DFD9-49F5-9A19-9AEE5B29341A/WebPlatformInstaller_x86_en-US.msi"
+
+echo "Unzipping WebPlatformInstaller Installer"
+start webplatforminstaller-installer.msi
+:: -----------------------------------------------------------------------------------------
+
+
+
+
+
+:: download `MySQL 5.7.26` installer
+echo "downloading MySQL version 8.0.16.0 installer"
+curl -o "mysql-installer8.0.16.0.msi" "https://dev.mysql.com/get/Downloads/MySQLInstaller/mysql-installer-web-community-8.0.16.0.msi"
+
+start mysql-installer8.0.16.0.msi
+:: -----------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+:: install `python` installer to run `python` utilities.
+echo "downloading Python installer installer..."
+curl -o "python-v3.7.3-installer.exe" "https://www.python.org/ftp/python/3.7.3/python-3.7.3-amd64.exe"
+
+echo "Installing python3"
+start python-v3.7.3-installer.exe
+:: -----------------------------------------------------------------------------------------
+
+
+
+
+
+
 :: install `Node` installer to create projects with `Node.js`
 echo "downloading NPM installer..."
-curl -o node-v10.16.0-x64.msi https://nodejs.org/dist/v10.16.0/node-v10.16.0-x64.msi
+curl -o "node-v10.16.0-x64.msi" "https://nodejs.org/dist/v10.16.0/node-v10.16.0-x64.msi"
 
 echo "Installing NodeJS"
 start node-v10.16.0-x64.msi
@@ -43,15 +107,46 @@ start node-v10.16.0-x64.msi
 
 
 
-:: install `python` installer to run `python` utilities.
-echo "downloading Python installer installer..."
-curl -o python-v3.7.3-installer.exe https://www.python.org/ftp/python/3.7.3/python-3.7.3-amd64.exe
 
-echo "Installing python3"
-start python-v3.7.3-installer.exe
+
+
+
+
+
+
+
+:: install `Notepad++` installer to run `Notepad++` utilities.
+echo "downloading Notepad++ installer installer..."
+curl -o "npp-installer.exe" "https://files.downloadnow-1.com/s/software/16/46/10/52/npp.7.7.1.Installer.exe?token=1563418694_6ce5e51e92378f5a924729ca21824e20&fileName=npp.7.7.1.Installer.exe"
+
+echo "Installing Notepad++"
+start npp-installer.exe
 :: -----------------------------------------------------------------------------------------
 
 
+
+
+
+
+:: install `VSCode` to edit code
+echo "downloading Visual Studio Code installer installer..."
+curl -o "vscode-installer.exe" "https://az764295.vo.msecnd.net/stable/2213894ea0415ee8c85c5eea0d0ff81ecc191529/VSCodeUserSetup-ia32-1.36.1.exe"
+
+echo "Installing Visual Studio Code"
+start vscode-installer.exe
+:: -----------------------------------------------------------------------------------------
+
+
+
+
+
+:: install `IntelliJ Idea` installer to edit projects
+echo "downloading IntelliJ installer..."
+curl -o intellij-community-v2019.1.3.exe https://download.jetbrains.com/idea/ideaIC-2019.1.3.exe
+
+echo "Installing Intellij"
+start intellij-community-v2019.1.3.exe
+:: -----------------------------------------------------------------------------------------
 
 
 
@@ -74,13 +169,6 @@ endlocal
 
 
 
-:: download `MySQL 5.7.26` installer
-echo "downloading MySQL version 5.7.26 installer"
-curl -o mysql-installer5.7.26.zip https://dev.mysql.com/get/Downloads/MySQL-5.7/mysql-5.7.26-winx64.zip
-
-echo "Unzipping MySQL version 5.7.26 installer"
-"C:\Program Files\7-Zip\7z.exe" x mysql-installer5.7.26.zip
-:: -----------------------------------------------------------------------------------------
 
 
 
@@ -88,49 +176,6 @@ echo "Unzipping MySQL version 5.7.26 installer"
 
 
 
-
-
-
-
-
-:: download PHP installer
-echo "downloading PHP installer"
-curl -o C:\php.zip https://windows.php.net/downloads/releases/php-7.2.20-Win32-VC15-x64.zip
-
-echo "Unzipping PHP Installer"
-"C:\Program Files\7-Zip\7z.exe" x C:\php.zip
-:: -----------------------------------------------------------------------------------------
-
-
-
-
-
-
-
-
-:: download `phpMyAdmin`
-echo "downloading phpMyAdmin installer"
-curl -o phpMyAdmin-v4.9.0.1.zip https://files.phpmyadmin.net/phpMyAdmin/4.9.0.1/phpMyAdmin-4.9.0.1-all-languages.zip
-
-echo "Unzipping phpMyAdmin installer"
-"C:\Program Files\7-Zip\7z.exe" x phpMyAdmin-v4.9.0.1.zip
-:: -----------------------------------------------------------------------------------------
-
-
-
-
-
-
-
-
-
-:: download `Apache2` installer
-echo "downloading Apache installer"
-curl -o apache-installer.zip https://dev.mysql.com/get/Downloads/MySQL-5.7/mysql-5.7.26-winx64.zip
-
-echo "Unzipping Apache Installer"
-"C:\Program Files\7-Zip\7z.exe" x apache-installer.zip
-:: -----------------------------------------------------------------------------------------
 
 
 
@@ -150,11 +195,6 @@ start iexplore.exe https://www.google.com/chrome/thank-you.html?statcb=1&install
 
 
 
-
-:: install `Notepad++` application to edit projects
-echo "downloading Notepad++ installer..."
-start iexplore.exe https://download.cnet.com/Notepad/3001-2352_4-10327521.html
-:: -----------------------------------------------------------------------------------------
 
 
 
@@ -181,23 +221,9 @@ start iexplore.exe https://aka.ms/win32-x64-user-stable
 
 
 
-:: unzip download
-echo "Unzipping files zipped files"
-"C:\Program Files\7-Zip\7z.exe" x mysql-installer5.7.26.zip
-"C:\Program Files\7-Zip\7z.exe" x apache-installer.zip
-:: -----------------------------------------------------------------------------------------
-
-
-
-
-
-
-
-
-
 
 :: open downloads directory
-start explorer "%userprofile%\Downloads"
+start explorer "%userprofile%\Downloads\sdk-dependencies\"
 :: -----------------------------------------------------------------------------------------
 
 
@@ -205,14 +231,4 @@ start explorer "%userprofile%\Downloads"
 
 
 
-
-
-
-:: install `IntelliJ Idea` installer to edit projects
-echo "downloading IntelliJ installer..."
-curl -o intellij-community-v2019.1.3.exe https://download.jetbrains.com/idea/ideaIC-2019.1.3.exe
-
-echo "Installing Intellij"
-start intellij-community-v2019.1.3.exe
-:: -----------------------------------------------------------------------------------------
 
